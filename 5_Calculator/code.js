45b41ad04d6901935cb8f12e8e96ac908fa5e772
Nacho <miguerra1987@gmail.com>
2024-10-02 00:55:56 -0300

    calculator

--- 5_Calculator/code.js
+++ 5_Calculator/code.js
@@ -29,21 +29,59 @@ function operate(num1,num2,operator){
   else if (operator === "*"){
     return multiply(num1,num2);
   }
-  else if (operator === "/"){
+  else if (operator === "/" && parseInt(num2) !== 0){
     return divide(num1,num2);
+  }
+  else if (operator === "/" && parseInt(num2) === 0){
+    return "Error: division by zero";
   }
 }
 
 let visorSpan = document.querySelector("#visor");
 const numButtons = document.querySelectorAll(".number-buttons");
+let numberTemp = "";
 
 numButtons.forEach((numButton) => {
        
   numButton.addEventListener("click", () => {
 
-    visorSpan.textContent = numButton.textContent;
+    numberTemp += numButton.textContent;
+    visorSpan.textContent = numberTemp;
 
   });
+});
+
+let counter = 0;
+
+const OperatorButtons = document.querySelectorAll(".number-operators");
+
+OperatorButtons.forEach((OperatorButton) => {
+       
+  OperatorButton.addEventListener("click", () => {
+
+    if (counter === 0){
+      number1 = parseFloat(numberTemp);
+      operator = OperatorButton.textContent;
+      numberTemp = "";
+      counter++;
+
+    }
+    else if (counter === 1){
+      number2 = parseFloat(numberTemp);
+      numberTemp = "";
+      visorSpan.textContent = operate(number1,number2,operator);
+      number1 = parseFloat(operate(number1,number2,operator));
+      operator = OperatorButton.textContent;
+      counter = 1;
+    }
+
+  });
+});
+
+document.querySelector("#equal").addEventListener("click", () => {
+  number2 = parseFloat(numberTemp);
+  visorSpan.textContent = operate(number1,number2,operator);
+
 });
 
 
@@ -51,5 +89,9 @@ const clearButton = document.querySelector("#clear");
 
 clearButton.addEventListener("click", () => {
   visorSpan.innerHTML = "";
+  numberTemp = "";
+  number1 = 0;
+  number2 = 0;
+  counter = 0;
 });
 
