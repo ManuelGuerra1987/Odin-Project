33be49bcf3b01034a77418e043be859e84f39546
Nacho <miguerra1987@gmail.com>
2024-10-15 01:02:59 -0300

    HashMap

--- 15_HashMap/code.js
+++ 15_HashMap/code.js
@@ -56,15 +56,10 @@ class HashMap{
     }
 
     hash(key) {
-        let hashCode = 0;
-           
-        const primeNumber = 31;
-        for (let i = 0; i < key.length; i++) {
-          hashCode = primeNumber * hashCode + key.charCodeAt(i);
-        }
-    
-        hashCode = hashCode % 20;
-     
+
+        const firstChar = key.charAt(0).toLowerCase();
+        const hashCode = firstChar.charCodeAt(0) - 'a'.charCodeAt(0);
+
         return hashCode;
       }
 
@@ -134,6 +129,86 @@ class HashMap{
 
       }
 
+      remove(key){
+
+        let index = this.hash(key);
+        let linkedList = this.buckets[index];
+
+        //1er case: empty bucket
+        if (linkedList === null) {
+            return false;
+        }
+        //2nd case: key is in first node
+        if (linkedList.head.key === key){
+            linkedList.head = linkedList.head.nextNode;
+            return true;
+        }
+        
+        //3rd case: key in the middle-end
+        let curr = linkedList.head;
+        let prev = linkedList.head;
+        curr = curr.nextNode;
+
+        while(curr !== null){
+            if(curr.key === key){
+                prev.nextNode = curr.nextNode;
+                return true;
+            }
+            curr = curr.nextNode;
+            prev = prev.nextNode;
+        }
+        return false;
+      }
+
+      keys(){
+
+        let keysList = []
+        for (let i = 0; i < this.length; i++){
+
+            if(this.buckets[i] !== null){
+                let ptemp = this.buckets[i].head;
+                while(ptemp !== null){
+                    keysList.push(ptemp.key);
+                    ptemp = ptemp.nextNode;
+                }
+            }
+        }
+        return keysList;
+      }
+
+      values(){
+
+        let valuesList = []
+        for (let i = 0; i < this.length; i++){
+
+            if(this.buckets[i] !== null){
+                let ptemp = this.buckets[i].head;
+                while(ptemp !== null){
+                    valuesList.push(ptemp.value);
+                    ptemp = ptemp.nextNode;
+                }
+            }
+        }
+        return valuesList;
+      }
+
+      entries(){
+
+        let entries = []
+        for (let i = 0; i < this.length; i++){
+
+            if(this.buckets[i] !== null){
+                let ptemp = this.buckets[i].head;
+                while(ptemp !== null){
+                    let entry = [ptemp.key,ptemp.value];
+                    entries.push(entry);
+                    ptemp = ptemp.nextNode;
+                }
+            }
+        }
+        return entries;
+      }
+
 
 }
 
@@ -144,9 +219,14 @@ let hashmap = new HashMap();
 hashmap.set("name","Nacho");
 hashmap.set("edad","37");
 hashmap.set("fruta","banana");
+hashmap.set("fruta2","manzana");
+hashmap.set("fruta3","anana");
 
-console.log(hashmap.has("frutas"));
-
+hashmap.remove("fruta2");
+hashmap.remove("fruta");
+console.log(hashmap.entries());
+hashmap.remove("edad");
+console.log(hashmap.entries());
 
 
 
