438dbbf285f2cf5c0881f7e6168c880c8525c59d
Nacho <miguerra1987@gmail.com>
2024-10-16 00:47:33 -0300

    bst step 3

--- 16_BinarySearchTree/bst.js
+++ 16_BinarySearchTree/bst.js
@@ -13,6 +13,36 @@ class Tree {
     }
 
     buildTree(array){
-        //todo
+
+        if (array.length === 0) return null;
+
+        const midpoint = Math.floor(array.length / 2);
+        const newNode = new Node(array[midpoint]);
+        newNode.leftChild = this.buildTree(array.slice(0, midpoint));
+        newNode.rightChild = this.buildTree(array.slice(midpoint + 1));
+        return newNode;
+
     }
 }
+
+let arr = [1,2,3,4,5,6,7,8,9];
+
+let test = new Tree(arr);
+
+let nodex = test.root;
+
+function prettyPrint (nodex, prefix = "", isLeft = true) {
+    if (nodex === null) {
+      return;
+    }
+    if (nodex.rightChild !== null) {
+      prettyPrint(nodex.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${nodex.value}`);
+    if (nodex.leftChild !== null) {
+      prettyPrint(nodex.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+
+
+  prettyPrint(nodex);
