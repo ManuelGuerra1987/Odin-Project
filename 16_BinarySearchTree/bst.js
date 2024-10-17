de7dbea9146350ba1956f8fa300fdae474978633
Nacho <miguerra1987@gmail.com>
2024-10-17 00:25:05 -0300

    insert method

--- 16_BinarySearchTree/bst.js
+++ 16_BinarySearchTree/bst.js
@@ -16,11 +16,61 @@ class Tree {
 
         if (array.length === 0) return null;
 
-        const midpoint = Math.floor(array.length / 2);
-        const newNode = new Node(array[midpoint]);
-        newNode.leftChild = this.buildTree(array.slice(0, midpoint));
-        newNode.rightChild = this.buildTree(array.slice(midpoint + 1));
-        return newNode;
+        let mid = Math.floor(array.length / 2);
+        let root = new Node(array[mid]);
+        let queue = [{node: root, start: 0, end: array.length-1}];
+        
+        while (queue.length > 0){
+
+          let {node, start, end} = queue.shift();
+          let mid = Math.floor((start + end) / 2);
+
+          //check if left subtree exists
+          if (start < mid){
+            let leftMid = Math.floor((start + mid - 1) / 2);
+            node.leftChild = new Node(array[leftMid]);
+            queue.push({node: node.leftChild, start: start, end: mid - 1});
+          }
+
+          //check if rigth subtree exists
+          if (end > mid){
+            let rightMid = Math.floor((mid+1+end) / 2);
+            node.rightChild = new Node(array[rightMid]);
+            queue.push({node: node.rightChild, start: mid + 1, end: end});
+          }
+        }
+
+        return root;
+
+    }
+
+    insert (value){
+
+      let currNode = this.root;
+
+      while (true){
+
+        if (value > currNode.value && currNode.rightChild !== null){
+          currNode = currNode.rightChild;
+        }
+
+        else if (value > currNode.value && currNode.rightChild === null){
+          let newNode = new Node(value);
+          currNode.rightChild = newNode;
+          break;
+        }
+        else if (value < currNode.value && currNode.leftChild !== null){
+          currNode = currNode.leftChild;
+        }
+        else if (value < currNode.value && currNode.leftChild === null){
+          let newNode = new Node(value);
+          currNode.leftChild = newNode;
+          break;
+        }
+        else{
+          break;
+        }
+      }
 
     }
 }
@@ -46,3 +96,4 @@ function prettyPrint (nodex, prefix = "", isLeft = true) {
 
 
   prettyPrint(nodex);
+
