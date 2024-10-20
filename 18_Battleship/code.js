b2586954fb02a14fbb70a6b4dede063a3460f3ba
Nacho <miguerra1987@gmail.com>
2024-10-20 00:39:07 -0300

    battleship step 2

--- 18_Battleship/code.js
+++ 18_Battleship/code.js
@@ -1,20 +1,85 @@
 class Ship{
-    constructor(length){
+    constructor(length,coordinates){
         this.length = length;
         this.hits = 0;
-        this.sunk = false;
+        this.coordinates = coordinates;
     }
 
     hit(){
-        if(!this.sunk){
+        if(!this.isSunk()){
             this.hits++;
         }
-        this.isSunk();
     }
 
     isSunk(){
         if(this.hits === parseInt(this.length)){
-            this.sunk = true;
+            return true;
         }
+        else{
+            return false;
+        }
+    }
+}
+
+class Gameboard{
+    constructor(){
+        this.board = Array.from({ length: 10 }, () => Array(10).fill(0));
+        this.ships = [];
+        this.missedShots = [];
+    }
+
+    placeShip(length,coordinates){
+
+        coordinates = coordinates;
+        let ship = new Ship(length,coordinates);
+        this.ships.push(ship);
+
+        for (let i = 0; i < length; i++){
+            let coordinate = coordinates[i];
+            let [row,col] = coordinate;
+            this.board[row][col] = 1;
+        }
+    }
+
+    receiveAttack (coordinate){
+
+        let [row,col] = coordinate;
+
+        if(this.board[row][col] === 1){
+
+            for (let ship of this.ships){
+                
+                for (let coord of ship.coordinates){
+
+                    if(coord[0] === row && coord[1] === col){
+
+                        ship.hit();
+                        return;
+                    }
+                }
+            }
+        }
+        else{
+            this.missedShots.push(coordinate);
+        }
+    }
+
+    hasShipsLeft(){
+
+        let shipsQ = this.ships.length;
+        let shipsSunked = 0;
+
+        for (let ship of this.ships){
+            if(ship.isSunk()){
+                shipsSunked++;
+            }
+        }
+        if (shipsQ === shipsSunked){
+            return false;
+        }
+        else{
+            return true;
+        }
+
     }
 }
