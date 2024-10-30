89ef27ef38f6c3466b7e594f7db2748567599d22
Nacho <miguerra1987@gmail.com>
2024-10-29 23:48:56 -0300

    controllers update

--- 24_MiniMessageBoard/controllers/messageController.js
+++ 24_MiniMessageBoard/controllers/messageController.js
@@ -1,15 +1,17 @@
 const messages = require('../db');
 
-const getMessage = (req, res) => {
+function getMessage (req, res) {
     
     const messageId  = parseInt(req.params.messageId);
-
     const message = messages[messageId];
 
     if (!message) {
         return res.status(404).send("Message not found");
     }
 
-    res.render("message", { message: message});};
+    res.render("message", { message: message});
+
+}
+
 
 module.exports = { getMessage };
--- 24_MiniMessageBoard/controllers/newController.js
+++ 24_MiniMessageBoard/controllers/newController.js
@@ -1,13 +1,11 @@
 const messages = require('../db');
 
-const postMessage = (req, res) => {
+function postMessage (req, res) {
     
     const newMessage = {text: req.body.messageText, user: req.body.messageUser, added: new Date()};
-
     messages.push(newMessage);
-  
     res.redirect("/");
 
-};
+}
 
 module.exports = { postMessage };
